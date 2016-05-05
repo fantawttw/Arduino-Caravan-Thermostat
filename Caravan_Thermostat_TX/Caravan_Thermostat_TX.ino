bbebecd187e39187d1421505e1615b6398d42e60
Jon Burrows <github@jonburrows.co.uk>
2016-05-05 10:23:44 +0100

    Basic sketch layout

---
+++ Caravan_Thermostat_TX/Caravan_Thermostat_TX.ino
@@ -0,0 +1,25 @@
+/* 
+ 
+Caravan Thermostat TX
+
+Copyright 2016 Jon Burrows
+
+ */
+
+// Setup the includes
+
+// Define the pins.
+
+// Create instances of the library
+
+// Define Global Variables
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

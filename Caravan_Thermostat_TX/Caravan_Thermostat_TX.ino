731b7095deca7272da804b7ccdf2a7e6351bc990
Jon Burrows <github@jonburrows.co.uk>
2016-05-05 10:46:52 +0100

    Added Basic definitions

--- Caravan_Thermostat_TX/Caravan_Thermostat_TX.ino
+++ Caravan_Thermostat_TX/Caravan_Thermostat_TX.ino
@@ -1,25 +1,48 @@
-/* 
- 
-Caravan Thermostat TX
+/*
 
-Copyright 2016 Jon Burrows
+  Caravan Thermostat TX
 
- */
+  Copyright 2016 Jon Burrows
+
+*/
 
 // Setup the includes
+#include <SPI.h>
+#include <TFT.h>  // Arduino LCD library
+#include <dht.h> // DHT11 Library
+#include <DS1307RTC.h> // RTC Library
+//#include <IRremote.h> // IR Library
 
 // Define the pins.
 
-// Create instances of the library
+// LCD
+#define cs   10
+#define dc   9
+#define rst  8
+
+// DHT11
+#define DHT11PIN 2
+
+// IR Receiver
+#define IRPIN 5
+
+// Create instances
+TFT TFTscreen = TFT(cs, dc, rst);
 
 // Define Global Variables
- 
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  TFTscreen.begin();
+  TFTscreen.background(0, 0, 0);
+  SetupDisplayLayout();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// Define Functions.
+void SetupDisplayLayout() {
+}
+
